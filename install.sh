a59cd437186b35bcf8df084fa929388fa075d47c
500PING <ndt512x@gmail.com>
2022-04-10 17:28:44 +0700

    finish

--- install.sh
+++ install.sh
@@ -3,16 +3,12 @@ apt-get purge --auto-remove snort -y
 rm /tmp/alert
 
 export DEBIAN_FRONTEND=noninteractive
-apt-get -yq install snort
+apt-get update
+apt-get -yq install snort=2.9.7.0-5build1 python3-pip
 
-# cp -R $1/* /opt
-# rm -rf /etc/snort
-# cp -R $1/SnortInstall/snort/ /etc/snort/
-# cp -TR /opt/SnortInstall/snort/rules /etc/snort/rules
-# rsync -a --delete $1/SnortInstall/snort/etc/ /etc/snort
-# rsync -a --delete $1/SnortInstall/snort/rules/ /etc/snort/rules
+pip3 install -r $1/SnortInstall/requirements.txt
 
-# touch /etc/snort/rules/white_list.rules
-# touch /etc/snort/rules/black_list.rules
+rm -rf /etc/snort
+cp -R $1/SnortInstall/snort/ /etc/snort/
 
 snort -D -A fast -l /tmp -c /etc/snort/snort.conf
--- main.py
+++ main.py
@@ -1,15 +1,41 @@
 import sys
 import src.greeter as greeter
-# import subprocess
+import time
+import awsiot.greengrasscoreipc
+import awsiot.greengrasscoreipc.client as client
+from awsiot.greengrasscoreipc.model import (
+    QOS,
+    PublishToIoTCoreRequest
+)
+from datetime import datetime
 
 def main():
     args = sys.argv[1:]
     if len(args) == 1:
         print(greeter.get_greeting(args[0]))
 
-    # subprocess.call(f"cp -TR {args[1]}/SnortInstall/snort/etc /etc/snort", shell=True)
-    # subprocess.call(f"cp -TR {args[1]}/SnortInstall/snort/rules /etc/snort/rules", shell=True)
-    # subprocess.call("snort -D -A fast -l /tmp -c /etc/snort/snort.conf", shell=True)
+    TIMEOUT = 10
+
+    ipc_client = awsiot.greengrasscoreipc.connect()
+                        
+    topic = "my/topic"
+    message = "Hello, World"
+    qos = QOS.AT_LEAST_ONCE
+    while True:
+        now = datetime.now()
+
+        current_time = now.strftime("%H:%M:%S")
+
+        request = PublishToIoTCoreRequest()
+        request.topic_name = topic
+        request.payload = bytes(f"{current_time}: {message}", "utf-8")
+        request.qos = qos
+        operation = ipc_client.new_publish_to_iot_core()
+        operation.activate(request)
+        future = operation.get_response()
+        future.result(TIMEOUT)
+        time.sleep(10)
+
     
 if __name__ == "__main__":
     main()
